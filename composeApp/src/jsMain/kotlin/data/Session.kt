b8797c047eb9bcc96e70c22b1610bebc24b0db69
Holy-Knight <vnycall74@naver.com>
2024-06-27 04:21:34 +0900

    feat: hardcoded csv session data

---
+++ composeApp/src/jsMain/kotlin/data/Session.kt
@@ -0,0 +1,10 @@
+package data
+
+data class Session(
+    val room: String,
+    val track: String,
+    val time: String,
+    val speaker: String,
+    val career: String,
+    val title: String
+)
